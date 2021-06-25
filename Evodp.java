e84f3b300c013a045e364a8bea1eb38730b81d6e
AWDCRJY19BCA1 <86476110+AWDCRJY19BCA1@users.noreply.github.com>
2021-06-25 20:46:25 +0530

    Create Evodp.java

---
+++ Evodp.java
@@ -0,0 +1,16 @@
+import java.io.*;
+class Evodp
+{
+public static void main (String st[])
+{
+     int p=15;
+      if(p&1!=0)
+      {
+         System.out.println("odd");
+      }
+      else
+      {
+         System.out.println("even");
+      }
+}
+}
