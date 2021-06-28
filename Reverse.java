e8870d0abbb8e7dedb131e5951d64984a3efc0b8
AWDCRJY19BCA1 <86476110+AWDCRJY19BCA1@users.noreply.github.com>
2021-06-28 21:36:58 +0530

    Create Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,13 @@
+import.java.io.*;
+public class Reverse
+{
+	public static void main(String[] args)
+  {
+      	int y=10;
+         	for(int x=40;x>=y;x--)
+       	if(x%2==0)
+	   {
+	     System.out.println(x+"");
+     	}
+}
+}
