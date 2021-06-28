3b2d0836cbdb647f97ffe4717fdfcb2404a55b04
AWDCRJY19BCA1 <86476110+AWDCRJY19BCA1@users.noreply.github.com>
2021-06-28 22:52:52 +0530

    Update Reverse1.java

--- Reverse1.java
+++ Reverse1.java
@@ -1,1 +1,19 @@
-
+import java.io.*;
+import java.util.*;
+public class Reverse
+{
+	public static void main(String[] args)
+  {
+      int x,y;
+    System.out.println("enter the low and high ranges");
+    Scanner s=new Scanner(System.in);
+    x=s.nextInt();
+    y=s.nextInt();           
+    while(x!=y)
+       	if(x%2==0)
+	   {
+	     System.out.println(x+"");
+          break;
+     	}
+}
+}
