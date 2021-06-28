f80218d0d2a8795aa69252fdea38fe2e8af851b4
AWDCRJY19BCA1 <86476110+AWDCRJY19BCA1@users.noreply.github.com>
2021-06-28 22:19:35 +0530

    Update Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -1,10 +1,10 @@
-import.java.io.*;
+import java.io.*;
 public class Reverse
 {
 	public static void main(String[] args)
   {
-      	int y=10;
-         	for(int x=40;x>=y;x--)
+      	int y=10;//lower range
+         	for(int x=40;x>=y;x--)//x is the highest range
        	if(x%2==0)
 	   {
 	     System.out.println(x+"");
