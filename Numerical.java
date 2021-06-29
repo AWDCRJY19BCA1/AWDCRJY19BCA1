eeb032270232e58ae58aefb51ba0b1b9fe3bf2b2
AWDCRJY19BCA1 <86476110+AWDCRJY19BCA1@users.noreply.github.com>
2021-06-29 21:43:18 +0530

    Create Numerical.java

---
+++ Numerical.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+public classs Numerical
+{
+public static void main(String[]args)
+{
+int []a=new int[10];
+System.out.println("Enter the number");
+Scanner 
+System.out.println("Odd Numbers----->");  
+for(int i=0;i<a.length;i++)
+{  
+if(a[i]%2!=0){  
+System.out.println(a[i]);  
+}  
+}  
+System.out.println("Even Numbers----->");  
+for(int i=0;i<a.length;i++)
+{  
+if(a[i]%2==0){  
+System.out.println(a[i]);  
+}  
+}  
+}
+}
